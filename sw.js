2973ee404116df912e217e340dcd26f1f5e65ce9
Maverick-GTO <irusanch@gmail.com>
2026-01-18 15:59:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,37 @@
-const CACHE_NAME = 'english-master-v3';
+const CACHE_NAME = 'english-master-v5';
 const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
   './sw.js',
   'https://img.icons8.com/ios-filled/512/ffffff/abc-block.png'
 ];
 
+// Installation
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
+// Aktivierung (Alten Cache löschen)
 self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+  self.clients.claim();
 });
 
+// Fetch (Wichtig für Chrome!)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
