2ace389465ac43b02da91697298e89f4634279f5
Maverick-GTO <irusanch@gmail.com>
2026-01-19 09:08:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,25 @@
-const CACHE_NAME = 'english-master-v5';
+const CACHE_NAME = 'english-v6';
 const ASSETS = [
-  './',
   './index.html',
   './manifest.json',
   './sw.js',
   'https://img.icons8.com/ios-filled/512/ffffff/abc-block.png'
 ];
 
-// Installation
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Aktivierung (Alten Cache löschen)
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-  self.clients.claim();
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
-// Fetch (Wichtig für Chrome!)
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
